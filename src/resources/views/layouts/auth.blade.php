c7372f1ac1606e0e81a6b1b7dc5fc44d7c12d3c3
Abdugoffor <abdugofforqodirov19@gmail.com>
2025-03-04 18:25:26 +0500

    asset

--- src/resources/views/layouts/auth.blade.php
+++ src/resources/views/layouts/auth.blade.php
@@ -9,7 +9,9 @@
 
     <!-- Global stylesheets -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-    <link href="{{ asset('global_assets/css/icons/icomoon/styles.min.css') }}" rel="stylesheet" type="text/css">
+
+    <!--localuchun uchun -->
+    {{-- <link href="{{ asset('global_assets/css/icons/icomoon/styles.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('assets/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <!-- /global stylesheets -->
 
@@ -19,9 +21,10 @@
     <!-- /core JS files -->
 
     <!-- Theme JS files -->
-    <script src="{{ asset('assets/js/app.js') }}"></script>
+    <script src="{{ asset('assets/js/app.js') }}"></script> --}}
 
-    {{-- <link href="{{ secure_asset('global_assets/css/icons/icomoon/styles.min.css') }}" rel="stylesheet" type="text/css">
+    <!--server uchun -->
+    <link href="{{ secure_asset('global_assets/css/icons/icomoon/styles.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ secure_asset('assets/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <!-- /global stylesheets -->
 
@@ -31,7 +34,7 @@
     <!-- /core JS files -->
 
     <!-- Theme JS files -->
-    <script src="{{ secure_asset('assets/js/app.js') }}"></script> --}}
+    <script src="{{ secure_asset('assets/js/app.js') }}"></script>
     <!-- /theme JS files -->
 
 </head>
