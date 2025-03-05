6f8fb85047b6f730cf0d596573e783de7a5d3b7b
Abdugoffor <abdugofforqodirov19@gmail.com>
2025-03-05 16:35:05 +0500

    qr dc ssse

--- src/resources/views/send_messages.blade.php
+++ src/resources/views/send_messages.blade.php
@@ -8,16 +8,7 @@
 <body>
     <h2>test uchun </h2>
     <h2>{{ $qrCode }}</h2>
-
-    <h2>{{ asset($qrCode) }}</h2>
-
-    <h2>{{ secure_asset($qrCode) }}</h2>
-
-    <img src="{{ $qrCode }}" alt="test">
-
-    <img src="{{ asset($qrCode) }}" alt="test">
-
-    <img src="{{ secure_asset($qrCode) }}" alt="test">
+    <a href="{{ config('app.url') }}/{{ $qrCode }}" target="_blank">qrCode</a>
 </body>
 
 </html>
