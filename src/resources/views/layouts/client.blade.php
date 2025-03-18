779e4c2852fc1670f9d75825ff9c0e49fa01cc75
Abdugoffor <abdugofforqodirov19@gmail.com>
2025-03-18 17:56:03 +0500

    path stylew

--- src/resources/views/layouts/client.blade.php
+++ src/resources/views/layouts/client.blade.php
@@ -5,18 +5,22 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Chess Olympiad 2025</title>
-    {{-- <link rel="icon" type="image/svg+xml" href="{{ secure_asset('frontend/assets/header_banner/chess_logo.svg') }}" />
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ secure_asset('frontend/assets/header_banner/chess_logo.svg') }}" /> --}}
-    <link rel="icon" type="image/svg+xml" href="{{ asset('frontend/assets/header_banner/chess_logo.svg') }}" />
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('frontend/assets/header_banner/chess_logo.svg') }}" />
+    
+    {{-- <link rel="icon" type="image/svg+xml" href="{{ asset('frontend/assets/header_banner/chess_logo.svg') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('frontend/assets/header_banner/chess_logo.svg') }}" /> --}}
+    
+    <link rel="icon" type="image/svg+xml" href="{{ secure_asset('frontend/assets/header_banner/chess_logo.svg') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ secure_asset('frontend/assets/header_banner/chess_logo.svg') }}" />
+
     <meta name="theme-color" content="#17a8d0" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300;400;500;600;700&display=swap"
         rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}" />
-    {{-- <link rel="stylesheet" href="{{ secure_asset('frontend/css/style.css') }}" /> --}}
+    
+    {{-- <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}" /> --}}
+    <link rel="stylesheet" href="{{ secure_asset('frontend/css/style.css') }}" />
 </head>
 
 <body>
@@ -123,8 +127,8 @@
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="{{ asset('frontend/js/main.js') }}"></script>
-    {{-- <script src="{{ secure_asset('frontend/js/main.js') }}"></script> --}}
+    {{-- <script src="{{ asset('frontend/js/main.js') }}"></script> --}}
+    <script src="{{ secure_asset('frontend/js/main.js') }}"></script>
 </body>
 <script>
     document.addEventListener("DOMContentLoaded", function() {
