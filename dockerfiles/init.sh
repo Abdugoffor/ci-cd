b958ad8b0d31f3d7fcc80bd40afaf789385945fa
Abdugoffor <abdugofforqodirov19@gmail.com>
2025-03-24 22:07:58 +0500

    sd3r4sdjihbgf

--- dockerfiles/init.sh
+++ dockerfiles/init.sh
@@ -20,7 +20,12 @@ php artisan route:cache
 php artisan view:cache
 
 # Установка прав на нужные папки
-# chmod -R 775 /var/www/laravel/storage /var/www/laravel/bootstrap/cache
+chmod -R 775 /var/www/laravel/storage /var/www/laravel/bootstrap/cache /var/www/laravel/public/uploaded
+chown -R www-data:www-data /var/www/laravel/storage /var/www/laravel/bootstrap/cache /var/www/laravel/public/uploaded
+chmod -R 775 /tmp
+chown -R www-data:www-data /tmp
+# Установка прав на нужные папки
+
 nohup php artisan queue:work --daemon > /var/www/laravel/storage/logs/queue.log 2>&1 &
 # Запуск PHP-FPM
 php-fpm
