0742d84a11027b2b29404a22bf28e328a0483298
Abdugoffor <abdugofforqodirov19@gmail.com>
2025-04-01 17:25:03 +0500

    fix

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use Illuminate\Container\Attributes\Auth;
+// use Illuminate\Container\Attributes\Auth;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -14,13 +15,14 @@ class LoginController extends Controller
 
     public function login(LoginRequest $request)
     {
-        if (Auth::attempt($request->only('email', 'password'))) {
-
+        if (auth()->attempt($request->only('email', 'password'))) {
+            
             $user = auth()->user();
-
+            
             isActive();
-
+            
             if (in_array($user->role, ['admin', 'user', 'moderator'])) {
+
                 return redirect()->route('application.index');
             }
 
