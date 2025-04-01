915b2e840b67fc8a9779673a9d2daa1e3ab72ece
Abdugoffor <abdugofforqodirov19@gmail.com>
2025-04-01 08:31:39 +0500

    fix: dbSeeder

--- dockerfiles/init.sh
+++ dockerfiles/init.sh
@@ -16,9 +16,9 @@ fi
 echo "Generating application key..."
 php artisan key:generate
 echo "Running migrations..."
-php artisan migrate:fresh --seed
+php artisan migrate:fresh
 echo "Running seeders..."
-# php artisan db:seed --class=DatabaseSeeder
+php artisan db:seed --class=DatabaseSeeder
 # php artisan db:seed --class=DatabaseSeeder
 
 # Keshlarni tozalash va yangilash
--- src/app/Models/Media.php
+++ src/app/Models/Media.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Media extends Model
 {
-    use SoftDeletes, HasHistory;
+    use HasHistory;
     protected $fillable = [
         'name',
         'title',
